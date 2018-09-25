5eb3de1aa526bf306c8fcac276d6dba535b2bac9
Darren Maddox <da1rren@gmail.com>
2018-09-25 11:53:29 +0100

    Replicated issue.

--- OData.InvalidOperationException/Startup.cs
+++ OData.InvalidOperationException/Startup.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNet.OData.Routing.Conventions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
 
 namespace OData.InvalidOperationException
 {
@@ -15,6 +19,7 @@ namespace OData.InvalidOperationException
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOData();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +30,31 @@ namespace OData.InvalidOperationException
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config.Count().Filter().OrderBy().Expand().Select().MaxTop(null);
+
+                var conventions = ODataRoutingConventions
+                    .CreateDefaultWithAttributeRouting("odata", config);
+
+                conventions.Insert(0, new RoutingConventions());
+
+                config.MapODataServiceRoute("odata", "odata", GetModel(), new DefaultODataPathHandler(), conventions);
             });
+        }
+
+        private static IEdmModel GetModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            return builder.GetEdmModel();
+        }
+    }
+
+    public class RoutingConventions : EntitySetRoutingConvention
+    {
+        public override string SelectAction(RouteContext routeContext, SelectControllerResult controllerResult, IEnumerable<ControllerActionDescriptor> actionDescriptors)
+        {
+            return base.SelectAction(routeContext, controllerResult, actionDescriptors);
         }
     }
 }
